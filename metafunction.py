91e7e9e60b7d08a784cec3b30f412c652af5595b
Matthew Brockley <67327540+mbrockley1901@users.noreply.github.com>
2020-07-07 15:39:16 -0400

    Added new metagenomes to pull from JGI

--- metafunction.py
+++ metafunction.py
@@ -2,9 +2,8 @@
 import os
 import time
 
-portals ={"LakEriepJuly2011":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/LakEriepJuly2011/download/_JAMO/51d5253b067c014cd6ef0e82/6308.7.42424.fastq.gz",
-	"NOAtaG_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG_7/download/_JAMO/55767a430d878529e7caf410/9146.3.123723.TGACCA.fastq.gz",
-	"NOAtaG_7_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG/download/_JAMO/55767a3e0d878529e7caf406/9146.2.123720.CGATGT.fastq.gz",} #add references to portals here after vewing xml file
+portals ={"NOAtaG_6_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG_4/download/_JAMO/55767a400d878529e7caf407/9146.2.123720.TTAGGC.fastq.gz",
+	 "NOAtaG_3_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG_2/download/_JAMO/5705dddf7ded5e2cf1e8f40b/10348.7.157979.GGACTCC-CTCTCTA.fastq.gz"} #add references to portals here after vewing xml file
 
 
 
