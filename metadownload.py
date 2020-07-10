7500e07286369b08dcb3e6719899f5c0cff81887
Brockley <mbrockley@ada.cm.cluster>
2020-07-09 23:10:01 -0400

    added .gitignore

--- metadownload.py
+++ metadownload.py
@@ -1,4 +1,4 @@
-
+"""metadownload.py"""
 import os
 
 portals = {"NOAtaG_3_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG_2/download/_JAMO/5705dddf7ded5e2cf1e8f40b/10348.7.157979.GGACTCC-CTCTCTA.fastq.gz", "NOAtaG_6_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/NOAtaG_4/download/_JAMO/55767a400d878529e7caf407/9146.2.123720.TTAGGC.fastq.gz", "LakEricontroER36_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/LakEricontroER36/download/_JAMO/5a8bfc8164d0b326cdd24cc2/12221.1.245265.ACTCGCT-TCGCATA.fastq.gz", "CB_VirMetaG_3_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&url=/CB_VirMetaG_2/download/_JAMO/59516afa7ded5e4e5bbdb64e/11724.6.216386.CTTGTA.fastq.gz"} #add references to portals here after vewing xml file
@@ -7,11 +7,11 @@ portals = {"NOAtaG_3_FD":"/portal/ext-api/downloads/get_tape_file?blocking=true&
 
 
 for key, value in portals.items():
-	#os.system("curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode 'login=mbrockley@middlebury.edu' --data-urlencode 'password=FILL IN' -c cookies > /dev/null")
+	os.system("curl 'https://signon.jgi.doe.gov/signon/create' --data-urlencode 'login=mbrockley@middlebury.edu' --data-urlencode 'password=Dumbledore{19}' -c cookies > /dev/null")
 	"""retrieves fasta file"""
 	command = "curl 'https://genome.jgi.doe.gov" + value + "' -b cookies > " + key + ".fastq.gz"
 	print(key)
 	print(command)
-	#os.system(command)
+	os.system(command)
 	print("")
 		
